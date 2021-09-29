25018be31400d1e94c37c85806270915c7a6efaa
Anthony Carr <kiidac@Anthonys-Air.lan>
2021-09-28 20:47:17 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -10,26 +10,11 @@ var userChoices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Var To Upper Case ---
-var blankUpper = [];
-var toUpper = function (x) {
-  return x.toUpperCase();
-};
-upperCase = lowerCase.map(toUpper);
+var upperCase = ['A', 'B', 'C'. 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 //-
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-//event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Start Function
 function generatePassword() {
@@ -143,4 +128,20 @@ function generatePassword() {
   return password;
 
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+//event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
 
